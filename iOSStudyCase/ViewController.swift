67b45ef3b5db02258327f0ed95b94cee6d938297
rasyidr298 <rasyidridla298@gmail.com>
2023-11-06 10:26:17 +0700

    Initial Commit

---
+++ iOSStudyCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSStudyCase
+//
+//  Created by Rasyid Ridla on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
